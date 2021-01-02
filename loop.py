afbdaf3fa31743b5992a6f09ed7a5fd41f63032c
jasekk99 <59716766+jasekk99@users.noreply.github.com>
2021-01-02 21:23:23 +0100

    Update loop.py

--- loop.py
+++ loop.py
@@ -2,10 +2,12 @@ import time
 while True:
     loopQuestion = input("run again? [Y/N]: ")
     if loopQuestion == "Y":
+        print("OK! Running the Application")
         import bvb_updates
         # wait 1 hour then continue
         time.sleep(1200)
         continue
     if loopQuestion == "N":
+        print("OK! Exiting loop")
         break
     break
