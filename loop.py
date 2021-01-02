6b2803dea495568a8543f6edad5d009b895ab472
jasekk99 <59716766+jasekk99@users.noreply.github.com>
2021-01-02 21:14:07 +0100

    Fixed loop

--- loop.py
+++ loop.py
@@ -1,7 +1,11 @@
+import time
 while True:
     loopQuestion = input("run again? [Y/N]: ")
     if loopQuestion == "Y":
         import bvb_updates
+        # wait 1 hour then continue
+        time.sleep(1200)
         continue
     if loopQuestion == "N":
         break
+    break
