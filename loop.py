079c0066f9312958ea7f42ace56457a59fa9cc9f
jasekk99 <59716766+jasekk99@users.noreply.github.com>
2021-01-03 23:57:33 +0100

    Update loop.py

--- loop.py
+++ loop.py
@@ -2,13 +2,13 @@ import time
 from colorama import init, Fore, Back
 while True:
     loopQuestion = input(f"{Fore.BLACK+Back.WHITE}run again? [Y/N]: {Fore.RESET+Back.RESET}")
-    if loopQuestion == "Y":
+    if loopQuestion == "Y" or loopQuestion == "y" or loopQuestion == "yes":
         print("OK! Running the Application")
         import bvb_updates
         # wait 1 hour then continue
         time.sleep(1200)
         continue
-    if loopQuestion == "N":
+    if loopQuestion == "N" or loopQuestion == "n" or loopQuestion == "no":
         print("OK! Exiting loop")
         break
     break
